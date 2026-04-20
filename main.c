9f6bc67b0ebb3d7f9d170956ffd80db023510d50
egor <egazam@mail.ru>
2026-04-20 17:36:55 +0700

    add code in main

--- main.c
+++ main.c
@@ -15,6 +15,9 @@ void printMx(){
 
 int main()
 {
-
+    int rows=3, columns=4, min=1, max=9;
+    int **Mx=createMx(rows, columns, min, max);
+    printMx(Mx,  rows, columns);
+    freeMx(Mx, rows);
     return 0;
 }
